5edf548d821e2ca7c5c17a7d29bbb5a2109cebe9
nerasan <salima.harun@gmail.com>
2021-01-08 23:43:55 -0600

    updated default about field to blank

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const User = mongoose.model(
        username: String,
        email: String,
        password: String,
-       about: { type: String, default: "default about field"},
+       about: { type: String, default: ""},
        roles: [
            {
                type: mongoose.Schema.Types.ObjectId,
