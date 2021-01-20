9c885cba09e040081685bbc55bc7c9c9bc640c41
epizarro98 <epizarro309@gmail.com>
2021-01-19 22:49:32 -0500

    added to auth and package.json

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
         // set header and allow use of x access token ( we will use this to pass our token )
         res.header(
             "Access-Control-Allow-Headers",
-            "x-access-token, Origin, Content-type, Accept"
+            "x-access-token, Origin, Content-type, Accept, x-requested-with"
         );
         next();
     })
